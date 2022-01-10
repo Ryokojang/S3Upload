ac6c63bab74d505f978c87c4d13509d795b57ecf
장우중 <juj3036>
2022-01-10 16:10:04 +0900

    - 유니티 이미지 업로드

--- Assets/Scripts/Write.cs
+++ Assets/Scripts/Write.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using UnityEngine.UI;
+using UnityEngine;
 using System.Runtime.Serialization.Formatters.Binary;
 
 public class Write : MonoBehaviour
@@ -13,6 +14,8 @@ public class Write : MonoBehaviour
     public InputField userName;
     public Text buttonText;
 
+    public Image avatar;
+    public Texture2D copy;
     public void UpdateText()
     {
         string path = "Assets/message.txt";
@@ -57,6 +60,28 @@ public class Write : MonoBehaviour
         }
     }
 
+    public void Button()
+    {
+        if (email.text == "" || userName.text == "")
+        {
+            buttonText.text = "Fill in Info Please";
+        }
+        else
+        {
+            copy = duplicateTexture((Texture2D)avatar.mainTexture);
+            UserInfo userInfo = new UserInfo()
+            {
+                email = email.text,
+                userName = userName.text,
+                //picture = copy.EncodeToPNG()
+                picture = copy.EncodeToJPG()
+            };
+
+            byte[] userInfoData = ObjectToByteArray(userInfo);
+            FindObjectOfType<AWSManager>().UploadFile(userInfoData, userName.text);
+        }
+    }
+
     private byte[] ObjectToByteArray(UserInfo obj)
     {
         BinaryFormatter bf = new BinaryFormatter();
@@ -64,5 +89,25 @@ public class Write : MonoBehaviour
         bf.Serialize(ms, obj);
 
         return ms.ToArray();
+    }
+
+    private Texture2D duplicateTexture(Texture2D source)
+    {
+        RenderTexture renderTex = RenderTexture.GetTemporary(
+            source.width,
+            source.height,
+            0, RenderTextureFormat.Default,
+            RenderTextureReadWrite.Linear
+            );
+
+        Graphics.Blit(source, renderTex);
+        RenderTexture previous = RenderTexture.active;
+        RenderTexture.active = renderTex;
+        Texture2D readableTex = new Texture2D(source.width, source.height);
+        readableTex.ReadPixels(new Rect(0, 0, renderTex.width, renderTex.height), 0, 0);
+        readableTex.Apply();
+        RenderTexture.active = previous;
+        RenderTexture.ReleaseTemporary(renderTex);
+        return readableTex;
     }
 }
